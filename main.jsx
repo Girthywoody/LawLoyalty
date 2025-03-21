1db377557381521201f17a76a11bc2ba8eed9604
Joshua Law <joshua-law@hotmail.com>
2025-03-21 01:06:25 -0400

    Simplify auth flow

--- main.jsx
+++ main.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import CompleteSignup from './CompleteSignup';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/complete-signup" element={<CompleteSignup />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
